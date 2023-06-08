73fca4fea612b83e8ab9555c37d8318b33a8164b
Aries Chang <chang.persistence@gmail.com>
2023-06-08 22:09:57 +0800

    initial commit

---
+++ ttinviter/src/main/java/com/social/ttinviter/TtinviterApplication.java
@@ -0,0 +1,13 @@
+package com.social.ttinviter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtinviterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtinviterApplication.class, args);
+	}
+
+}
---
+++ ttinviter/src/test/java/com/social/ttinviter/TtinviterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.ttinviter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtinviterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
